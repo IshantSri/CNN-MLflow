758a665599237a0062ede67d9453ec0a60f0eb6b
ishant <ishantsrivastava386@gmail.com>
2022-07-24 23:33:53 +0530

    data loading & mdm

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -4,6 +4,7 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories,unzip_file
+from src.utils.mdm import  val_data
 import random
 import urllib.request as req
 
@@ -17,12 +18,12 @@ logging.basicConfig(
     )
 
 
-def main(config_path, unzip_data=None):
+def main(config_path):
     ## read config files
     config = read_yaml(config_path)
-    url = config["data"]["url"]
+    url = config["data"]["url"] #in config.yaml(it is a nested dict) find value of url(a key) in data (which is a key) and store the url
 
-    local_dir = config["data"]["local_dir"]
+    local_dir = config["data"]["local_dir"]#get the local dirct name
     create_directories([local_dir])
 
     data_file= config["data"]["data_file"]
@@ -35,12 +36,22 @@ def main(config_path, unzip_data=None):
     else:
         logging.info(f"file:{data_file} already present")
 
-    unzip_data_dir = config['data']['unzip_data']
-    create_directories([unzip_data_dir])
-    unzip_data_dir_path = os.path.join(local_dir)
-    logging.info(f"started unzipping data at {unzip_data_dir_path}")
-    unzip_file(source = data_file_path,dest = unzip_data_dir_path )
-    logging.info(f"data unzipping completed")
+    unzip_data_dir = config['data']['unzip_data_dir']
+    if os.path.exists(unzip_data_dir):
+        logging.info(f"data ecxtraction started")
+        create_directories([unzip_data_dir])
+        unzip_data_dir_path = os.path.join(local_dir)
+        unzip_file(source = data_file_path,dest = unzip_data_dir_path )
+    else:
+        logging.info(f"data already extracted")
+
+    # validating data
+    logging.info("starting validation")
+    val_data(config)
+    logging.info("data validation completed")
+
+
+
 
 
 
--- src/utils/common.py
+++ src/utils/common.py
@@ -5,6 +5,7 @@ import time
 import pandas as pd
 import json
 from zipfile import ZipFile
+
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
